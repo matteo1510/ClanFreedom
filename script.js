2b26c7b657df180b7e5d4b2b425d1e7ec5209f0e
matteo1510 <matteo.pibiri13@gmail.com>
2026-03-15 12:28:17 +0100

    Add animated counter and app opening functionality

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
-document.getElementById("appButton").onclick = function(){
+// contatore animato
+
+let counter = document.getElementById("counter");
+
+let value = 0;
+
+let interval = setInterval(() => {
+
+value++;
+
+counter.innerText = value;
+
+if(value >= 120){
+clearInterval(interval);
+}
+
+},30);
+
+
+// apertura app
+
+document.getElementById("android").onclick = function(){
 
 window.open("https://play.google.com/store/apps/details?id=com.floraincognita.app.floraincognita");
+
+}
+
+document.getElementById("ios").onclick = function(){
+
+window.open("https://apps.apple.com/app/flora-incognita/id1297860122");
 
 }
